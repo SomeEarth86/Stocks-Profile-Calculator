dd6f571329c3d1c70c3841f09f19f49a1c6a4261
Samarth Krishna Joshi <skrishnaj86@gmail.com>
2021-08-25 22:38:49 +0530

    fix: roundoff issue

--- app.js
+++ app.js
@@ -6,35 +6,33 @@ var resultButton = document.querySelector('#resultbtn');
 
 resultButton.addEventListener('click', getResults);
 
-function getResults(){
+function getResults() {
 
     // console.log(typeof initialPrice.value);
     var initialP = Number(initialPrice.value);
     var qty = Number(qtyStocks.value);
     var curr = Number(currentPrice.value);
 
-    if( initialP && qty && curr){
-        calculateProfitLoss(initialP,qty,curr);
+    if (initialP && qty && curr) {
+        calculateProfitLoss(initialP, qty, curr);
     }
-    
+
 }
 
-function calculateProfitLoss(initialP, qty,curr){
-    if(curr>initialP){
-        var profit = (curr - initialP) * qty ;    
-        var profitPercentage = ((profit/initialP)*100).toFixed(2);
+function calculateProfitLoss(initialP, qty, curr) {
+    if (curr > initialP) {
+        var profit = (curr - initialP) * qty;
+        var profitPercentage = ((profit / initialP) * 100).toFixed(2);
         showMessage(`Yay, You made profit of ${profit} and the Profit percentage is ${profitPercentage}%`);
-    }
-    else if(curr< initialP){
+    } else if (curr < initialP) {
         var loss = initialP - curr;
-        var lossPercentage =  ((loss/initialP) *100).toFixed(2);
+        var lossPercentage = ((loss / initialP) * 100).toFixed(2);
         showMessage(` Oops, You made loss of ${loss} units on stocks, the loss percentage is ${lossPercentage}%`);
-    }
-    else{
+    } else {
         showMessage("No pain no gain");
     }
 }
 
-function showMessage(msg){
+function showMessage(msg) {
     resultDisplay.innerText = msg;
 }
